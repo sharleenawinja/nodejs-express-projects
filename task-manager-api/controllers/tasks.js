a039d64564166f02f9036ed2e92f3fe82146fd66
sharleenawinja <awinjasharleen@gmail.com>
2023-04-27 15:46:09 +0300

    schema and validation for creating tasks

--- task-manager-api/controllers/tasks.js
+++ task-manager-api/controllers/tasks.js
@@ -1,3 +1,5 @@
+const Task = require("../models/task");
+
 const getAllTasks = (req, res) => {
   res.send("get all tasks");
 };
@@ -6,9 +8,13 @@ const getSingleTask = (req, res) => {
   res.send("get single task");
 };
 
-const createTask = (req, res) => {
-  //   res.json(req.body);
-  res.send("create task");
+const createTask = async (req, res) => {
+  try {
+    const task = await Task.create(req.body); //when you have an asynchronous function always do error handling eg with a try catch block
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const updateTask = (req, res) => {
